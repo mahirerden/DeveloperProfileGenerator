0df99c8ec000b824db0e33e070f1f64dcfe166ce
Mahir Erden <mahirerden@gmail.com>
2020-01-08 00:39:16 -0500

    homework is ready to submit

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# DeveloperProfileGenerator
+# Developer Profile Generator
+
+This is a command-line application which can be invoked with the following code.
+
+```sh
+node index.js
+```
+The PDF and HTML files will be populated with the following:
+
+* Profile image
+* User name
+* Links to the following:
+  * User location via Google Maps
+  * User GitHub profile
+  * User blog
+* User bio
+* Number of public repositories
+* Number of followers
+* Number of GitHub stars
+* Number of users following
+
+![Alt Text](./assets/img/terminal.png)
+
+The application generate PDF file with using Html-Pdf Npm module.
+Firts profile.Html generated then profile.pdf file will be generated dynamically.
+
+![Alt Text](./assets/img/profileHtml.png)
+
+![Alt Text](./assets/img/profilePDF.png)
+
+The background color of the generated PDF matches the color that the user provides with the below code.
+
+```sh
+const colors = {
+  indigo_blue: {
+    wrapperBackground: "indigo",
+    headerBackground: "royalblue",
+    headerColor: "white",
+    photoBorderColor: "white"
+  },
+  purple_salmon: {
+    wrapperBackground: "royalblue",
+    headerBackground: "salmon",
+    headerColor: "white",
+    photoBorderColor: "red"
+  },
+  orchid_thistle: {
+    wrapperBackground: "orchid",
+    headerBackground: "thistle",
+    headerColor: "black",
+    photoBorderColor: "slateblue"
+  },
+  gray_ligthgray: {
+    wrapperBackground: "slategray",
+    headerBackground: "lightgray",
+    headerColor: "black",
+    photoBorderColor: "black"
+  }
+};
+```
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-var http = require('http');
+const axios = require("axios");
 var generateHTML = require('./generateHTML');
+var pdf = require('html-pdf');
+var options = { format: 'Letter' };
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -10,35 +12,98 @@ function promptUser() {
      return inquirer.prompt([
           {
                type: "list",
-               message: "Please select header color",
-               name: "header-color",
+               message: "Please select template color",
+               name: "color",
                choices: [
-                    "green",
-                    "blue",
-                    "pink",
-                    "red"
+                    "indigo_blue",
+                    "purple_salmon",
+                    "orchid_thistle",
+                    "gray_ligthgray"
                ]
           },
           {
                type: "input",
                name: "name",
-               message: "What is your name?"
+               message: "What is your name? "
           },
-
+          {
+               type: "input",
+               name: "currentjob",
+               message: "What is your current job? "
+          },
+          {
+               type: "input",
+               name: "location",
+               message: "Where are you from? "
+          },
+          {
+               type: "input",
+               name: "githubname",
+               message: "What is GitHub UserName? "
+          },
+          {
+               type: "input",
+               name: "linkedin",
+               message: "Enter your LinkedIn UserName: "
+          },
+          {
+               type: "input",
+               name: "bio",
+               message: "Enter your bio: "
+          }
      ]);
+};
+
+async function init() {
+     console.log("hi")
+     try {
+          const answers = await promptUser();
+
+          var queryUrl = `https://api.github.com/users/${answers.githubname}/repos`;
+          await axios.get(queryUrl).then(res => {
+               answers.publicRepo = res.data.length;
+               //console.log(answers.publicRepo);
+          });
+
+          queryUrl = `https://api.github.com/users/${answers.githubname}/followers`;
+          await axios.get(queryUrl).then(res => {
+               answers.followers = res.data.length;
+               //console.log(answers.followers);
+          });
+          queryUrl = `https://api.github.com/users/${answers.githubname}/following`;
+          await axios.get(queryUrl).then(res => {
+               answers.following = res.data.length;
+               //console.log(answers.following);
+          });
+          queryUrl = `https://api.github.com/users/${answers.githubname}/starred`;
+          await axios.get(queryUrl).then(res => {
+               answers.starred = res.data.length;
+               //console.log(answers.starred);
+          });
+          queryUrl = `https://api.github.com/users/${answers.githubname}`;
+          await axios.get(queryUrl).then(res => {
+               var id = res.data.id;
+               //console.log(id);
+               answers.imgUrl = `https://avatars2.githubusercontent.com/u/${id}?v=4`;
+               //console.log(answers.imgUrl);
+          });
+
+          const html = generateHTML.generate(answers);
+
+          await writeFileAsync("profile.html", html);
+
+          var htmlPdf = fs.readFileSync('./profile.html', 'utf8');
+
+          await pdf.create(htmlPdf, options).toFile('./profile.pdf', function(err, res) {
+               if (err) return console.log(err);
+               console.log(res);
+          }); 
+
+          console.log("Successfully wrote to profile.html");
+          console.log("Successfully generated profile.pdf");
+     } catch (err) {
+          console.log(err);
+     }
 }
 
-
-
-// const PORT=8080; 
-
-// fs.readFile('./index.html', function (err, html) {
-
-//     if (err) throw err;    
-
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(PORT);
-// });
+init();
